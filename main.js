9885e3b6c6578a0df6c24a91b1c8d0be3760022f
chirag <99802739+chirag1132@users.noreply.github.com>
2022-03-17 15:14:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload() {
+}
+function setup(){
+    canvas = createCanvnas(300,300);
+    canvas.center();
+}
+function draw(){
+}
+function take_snapshot(){
+    save("my_picture.png");
+}
